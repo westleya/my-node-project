90a2655e3a4c656e5b5607efe92da549dc337592
westleya <u0543839@utah.edu>
2025-01-14 21:08:38 -0700

    Added JS -> Python communication

--- processor.js
+++ processor.js
@@ -4,6 +4,21 @@ function processData(jsonData){
     // Example processing logic
     console.log("Processing data:", jsonData);
 
+    
+    var spawn = require('child_process').spawn,
+    py    = spawn('python', ['compute_input.py']),
+    data = [1,2,3,4,5,6,7,8,9],
+    dataString = '';
+
+    py.stdout.on('data', function(data){
+        dataString += data.toString();
+    });
+    py.stdout.on('end', function(){
+        console.log('Sum of numbers=',dataString);
+    });
+    py.stdin.write(JSON.stringify(data));
+    py.stdin.end();
+
     // Add your processing logic here
     if (jsonData.message){
         return {success: true, processedMessage: jsonData.message.toUpperCase()};
